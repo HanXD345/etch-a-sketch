843559beab00cd9c23ac30d8007a3dba06256299
HanXD345 <manpreet345@yahoo.com>
2025-05-18 18:06:33 -0400

    Add attributes to divs

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 12; i++) {
     const rowContainer = document.createElement("div");
     rowContainer.className = "row-container";
-    for (let j = 0; j < 10; j++) {
+    for (let j = 0; j < 12; j++) {
         const boxContainer = document.createElement("div");
         boxContainer.className = "box-container"
         rowContainer.append(boxContainer);
