b4aba7d5a968ef4232efee3a14885cfda108ad89
HanXD345 <manpreet345@yahoo.com>
2025-05-18 18:18:29 -0400

    Create addEventListener for boxes
    Added 'addEventListener's to the boxes in the project and created it in
    such a way that, when the mouse hovers over the boxes, it changes the
    background color of the boxes to an arbitrary color.

--- script.js
+++ script.js
@@ -5,8 +5,17 @@ for (let i = 0; i < 12; i++) {
     rowContainer.className = "row-container";
     for (let j = 0; j < 12; j++) {
         const boxContainer = document.createElement("div");
-        boxContainer.className = "box-container"
+        boxContainer.className = "box-container";
         rowContainer.append(boxContainer);
     };
     container.append(rowContainer);
 };
+
+const boxContainer = document.querySelectorAll(".box-container");
+
+for (let box of boxContainer) {
+    console.log(box)
+    box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = "blue";
+    })
+}
